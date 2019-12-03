77274138de1e2e3c22d8a4251a10b5e32fc48a9c
Callmeden <maxdenn1995@mail.ru>
2019-12-03 17:58:54 +0300

    Хуан Хуанович
    Хуан Хуанович
    В метод main первым параметром приходит имя файла.
    В этом файле каждая строка имеет следующий вид:
    имя день месяц год
    где [имя] - может состоять из нескольких слов, разделенных пробелами, и имеет тип String.
    [день] - int, [месяц] - int, [год] - int
    данные разделены пробелами.
    
    Заполнить список PEOPLE используя данные из файла.
    Закрыть потоки.
    
    Пример входного файла:
    Иванов Иван Иванович 31 12 1987
    Вася 15 5 2013
    
    
    Требования:
    1. Класс Solution должен содержать публичную константу PEOPLE типа List<Person>, которая должна быть сразу проинициализирована.
    2. Программа НЕ должна считывать данные с консоли.
    3. Программа должна считывать содержимое файла (используй FileReader).
    4. Поток чтения из файла (FileReader) должен быть закрыт.
    5. Программа должна заполнить список PEOPLE данными из файла.
    6. Программа должна правильно работать с двойными именами, например Анна-Надежда.

---
+++ task1921.java
@@ -0,0 +1,52 @@
+package com.javarush.task.task19.task1921;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/* 
+Хуан Хуанович
+*/
+
+public class Solution {
+    public static final List<Person> PEOPLE = new ArrayList<Person>();
+
+    public static void main(String[] args) throws IOException, ParseException {
+        BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+
+        while(reader.ready()){
+            String[] s = reader.readLine().split("(?<=\\D)\\s(?=[0-9\\s])"); //ищем пробел, слева от которого не число а справа числа и пробелы
+            Date date = new SimpleDateFormat("dd MM yyyy").parse(s[1]);
+            PEOPLE.add(new Person(s[0],date));
+        }
+        reader.close();
+
+    }
+}
+//person.java :
+package com.javarush.task.task19.task1921;
+
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private Date birthDate;
+
+    public Person(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+}
