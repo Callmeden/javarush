561827f86c5c2056d7faa143f221eb8fa4f57826
Callmeden <maxdenn1995@mail.ru>
2019-12-05 22:08:30 +0300

    перегрузка, полиморфизм, абстракция, интерфейсы.
    Основы ООП: перегрузка, полиморфизм, абстракция, интерфейсы.

---
+++ task12/task1208/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+public static void print(int a){};    //Напишите тут ваши методы
+public static void print(boolean a){};    //Напишите тут ваши методы
+public static void print(String a){};    //Напишите тут ваши методы
+public static void print(float a){};    //Напишите тут ваши методы
+public static void print(Integer a){};    //Напишите тут ваши методы
+}
