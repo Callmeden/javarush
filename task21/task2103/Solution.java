0d60b7012c7d1a46312c1f85996550fb6f6fd4ab
Callmeden <maxdenn1995@mail.ru>
2019-12-11 17:58:12 +0300

    Устройство объекта Object
    Устройство объекта Object: equals, hashCode, clone, wait, notify, toString()

---
+++ task21/task2103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task21.task2103;
+//максимально упростить return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d) в методе calculate;
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
