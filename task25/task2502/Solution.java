95dddddbdf388c39c7de8a885c6b9ccc52cdac8a
Callmeden <maxdenn1995@mail.ru>
2019-12-22 22:15:25 +0300

    Создание и остановка нитей
    Создание и остановка нитей: start, interrupt, sleep, yield.

---
+++ task25/task2502/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws IllegalAccessException {
+            String[] strings = loadWheelNamesFromDB();
+            wheels=new ArrayList<>();
+            if(strings.length != Wheel.values().length) throw new IllegalAccessException();
+                for(String s:strings)
+                    wheels.add(Wheel.valueOf(s));
+
+            //init wheels here
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
