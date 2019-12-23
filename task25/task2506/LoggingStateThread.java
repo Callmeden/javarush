951d24aaab3594f5401dde70c83df0465d61e647
Callmeden <maxdenn1995@mail.ru>
2019-12-23 17:03:53 +0300

    Создание и остановка нитей
    Создание и остановка нитей: start, interrupt, sleep, yield.

---
+++ task25/task2506/LoggingStateThread.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task25.task2506;
+
+public class LoggingStateThread extends Thread {
+    private Thread thread;
+    @Override
+    public void run() {
+        State state, lastState = null;
+        do {
+            state = thread.getState();
+            if (state != lastState) {
+                System.out.println(state);
+                lastState = state;
+            }
+        } while (state != State.TERMINATED);
+    }
+
+    public LoggingStateThread(Thread thread) {
+        this.thread=thread;
+    }
+}
