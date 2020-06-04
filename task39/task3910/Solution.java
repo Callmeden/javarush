4f7332844071ac635e0458d2d74a26f41d7b4ca1
Callmeden <maxdenn1995@mail.ru>
2020-06-04 16:14:54 +0300

    Первое веб приложение. Работа с Tomcat и Idea
    Создаем первое веб приложение. Работа с Tomcat и Idea

---
+++ task39/task3910/Solution.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task39.task3910;
+
+/* 
+isPowerOfThree
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(-3)); // false
+        System.out.println(isPowerOfThree(-1)); // false
+        System.out.println(isPowerOfThree(0));  // false
+        System.out.println(isPowerOfThree(6));  // false
+        System.out.println(isPowerOfThree(18)); // false
+        System.out.println(isPowerOfThree(30)); // false
+        System.out.println("============================");
+        System.out.println(isPowerOfThree(1));  // true
+        System.out.println(isPowerOfThree(3));  // true
+        System.out.println(isPowerOfThree(9));  // true
+        System.out.println(isPowerOfThree(27)); // true
+    }
+
+    public static boolean isPowerOfThree(int n) {
+        if(n%3==0 && n!=0)
+            return isPowerOfThree(n/3);
+        else return n==1;
+    }
+}
